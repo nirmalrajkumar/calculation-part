3a13920d947ab5712b3d22c7e1f88b733dc9ed61
Nirmal RajKumar <nirmalrajkumar.b@asmltd.com>
2016-12-21 12:25:15 +0530

    done

---
+++ cal1.py
@@ -0,0 +1,17 @@
+var1= input("enter the first number: ")
+var= raw_input("enter the sign for calculation: ")
+var2= input("enter the second number: ")
+if var=='+':
+	print(var1+var2)
+elif var=='-':
+	print(var1-var2)
+elif var=='/':
+	print(var1/var2)
+elif var=='*':
+	print(var1*var2)
+else:
+	print("invalid")
+
+
+
+
